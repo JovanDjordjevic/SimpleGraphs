f98e545fe555fd23ae9851e8ccdeb353b78bd9eb
Jovan Djordjevic <joca.dj@hotmail.com>
2021-11-18 09:38:14 +0100

    Implemented basic API functions and << overload

--- SimpleGraphs.hpp
+++ SimpleGraphs.hpp
@@ -1,18 +1,23 @@
 #ifndef __SIMPLE_GRAPHS__
 #define __SIMPLE_GRAPHS__
 
+#include <iostream>
 #include <unordered_map>
 #include <vector>
 #include <optional>
 
+#define GRAPH_ERROR(message) std::cerr << "ERROR: " << message << std::endl; 
+
 //------------------------------------- API -------------------------------------
 namespace GraphClasses {
     enum class GraphType {   
+        Unset, 
         Directed,
         Undirected
     };
 
     enum class GraphWeights {   
+        Unset,
         Weighted,
         Unweighted
     };
@@ -20,10 +25,11 @@ namespace GraphClasses {
     template <typename DataType, typename WeightType>
     class Graph {
         public:
-            Graph(GraphType graphType, GraphWeights graphWeights);
+            Graph(GraphType graphType = GraphType::Unset, GraphWeights graphWeights = GraphWeights::Unset);
 
             void configureDirections(GraphType graphType);
             void configureWeights(GraphWeights graphWeights);
+            bool isConfigured();
 
             void readFromTxt(const char* filePath);
             void writeToTxt(const char* filePath);
@@ -33,6 +39,9 @@ namespace GraphClasses {
             void addEdge(DataType startNode, DataType neighborNode, WeightType edgeWeight); // for weighted graphs
             
             // ...
+
+            template<typename DataType, typename WeightType> 
+            friend std::ostream& operator<<(std::ostream& out, const GraphClasses::Graph<DataType, WeightType>& g);
             
         private:
             GraphType m_graphType;
@@ -73,7 +82,87 @@ namespace GraphAlgorithms {
 
 //------------------------------------- IMPLEMENTATION -------------------------------------
 namespace GraphClasses {
-    // ....
+    template<typename DataType, typename WeightType>
+    std::ostream& operator<<(std::ostream& out, const GraphClasses::Graph<DataType, WeightType>& g) {   
+        for(auto& kvPair : g.m_neighbors) {   
+            out << "Node [" << kvPair.first << "] has neighbours:" << std::endl;
+
+            if (g.m_graphWeights == GraphWeights::Weighted) {
+                for(auto&  val : kvPair.second) {
+                    out << "|\t [" << val.neighbor << "], edge weight: " << val.weight.value() << std::endl;
+                }
+            }
+            else {
+                for(auto&  val : kvPair.second) {
+                   out << "|\t [" << val.neighbor << "]" << std::endl;
+                }
+            }
+        }
+        return out;
+    }
+
+    template <typename DataType, typename WeightType>
+    Graph<DataType, WeightType>::Graph(GraphType graphType, GraphWeights graphWeights)
+        : m_graphType(graphType), m_graphWeights(graphWeights) {}
+
+    template <typename DataType, typename WeightType>
+    void Graph<DataType, WeightType>::configureDirections(GraphType graphType) {
+        m_graphType = graphType;
+    }
+
+    template <typename DataType, typename WeightType>
+    void Graph<DataType, WeightType>::configureWeights(GraphWeights graphWeights) {
+        m_graphWeights = graphWeights;
+    }
+
+    template <typename DataType, typename WeightType>
+    bool Graph<DataType, WeightType>::isConfigured() {
+        if (m_graphType != GraphType::Unset || m_graphWeights != GraphWeights::Unset) {
+            return true;
+        }
+        return false;
+    }
+
+    // samo ubacuje cvor, bez ikakvih grana i tezina
+    template <typename DataType, typename WeightType>
+    void Graph<DataType, WeightType>::addNode(DataType node) {
+        m_neighbors[node];
+    }
+
+    // mozda spoji oba addEdge overloada u jednu funkc
+    template <typename DataType, typename WeightType>
+    void Graph<DataType, WeightType>::addEdge(DataType startNode, DataType neighborNode) {
+        if (m_graphWeights == GraphWeights::Weighted)
+        {
+            GRAPH_ERROR("Graph is weighed and edge weight must be specified! ");
+            exit(EXIT_FAILURE);
+        }
+
+        if (m_graphType == GraphType::Directed) {
+            m_neighbors[startNode].emplace_back(neighborNode);
+        }
+        else { // undirected
+            m_neighbors[startNode].emplace_back(neighborNode);
+            m_neighbors[neighborNode].emplace_back(startNode);
+        }
+    }
+
+    template <typename DataType, typename WeightType>
+    void Graph<DataType, WeightType>::addEdge(DataType startNode, DataType neighborNode, WeightType edgeWeight) {
+        if (m_graphWeights == GraphWeights::Unweighted)
+        {
+            GRAPH_ERROR("Graph is not weighed but you are trying to specify edge weight!");
+            exit(EXIT_FAILURE);
+        }
+
+        if (m_graphType == GraphType::Directed) {
+            m_neighbors[startNode].emplace_back(neighborNode, edgeWeight);
+        }
+        else { // undirected
+            m_neighbors[startNode].emplace_back(neighborNode, edgeWeight);
+            m_neighbors[neighborNode].emplace_back(startNode, edgeWeight);
+        }
+    }
 
 } //namespace GraphClasses
 
